d68c7a1de3010d99d29b01444246db0fc3502402
GabrielAntunes34 <82909777+GabrielAntunes34@users.noreply.github.com>
2024-12-09 22:46:55 -0300

    Updating the headers of tile and tilemap
    It's more concise, but still not complete, or working

--- include/Tile.hpp
+++ include/Tile.hpp
@@ -1,23 +1,29 @@
 #ifndef TILE_H
 #define TILE_H
 
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+#define PLAYER 0
+#define ENEMY 1
+#define GROUND 2
+#define OBSTACLE 3
+
 class Tile {
     private:
         int id;
         bool tangibility;
-        // float pathfinding???
-
-    public:
-        Tile(int id = -1, bool tangibility = false)
-        : id(id), tangibility(tangibility) {};
-
+        Sprite sprite;
+        
         // Getters
         int getId();
         bool isTangible();
+        Sprite getSprite();
 
         // Setters
         void setTaniblity(bool tangibility);
         void setId(int id);
+        void setSprite();
 
         // Mechanics
         // CalculatePath???
--- include/TileMap.hpp
+++ include/TileMap.hpp
@@ -1,7 +1,12 @@
 #ifndef TILEMAP_H
 #define TILEMAP_H
 
+#include <iostream>
+#include <utility>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <SFML/Graphics.hpp>
 #include "Tile.hpp"
 
@@ -9,18 +14,26 @@
 #define TILE_MAP_H 22
 
 using namespace sf;
+using namespace std;
 
 class TileMap {
     private:
-        std::vector<std::vector<Tile>> tiles;   // Matriz de tiles lógicos
-        VertexArray vertexArray;                // Array do mapa a ser renderizado
-        Texture tileAtlas;                      // Referencia a imagem, na memória, com todos os sprites
-        Vector2u screenSize;                    // Tamaho da tela, para ajuste do aspect ratio
+        vector<vector<Tile>> tiles;         // Matriz de tiles lógicos
+        //VertexArray vertexArray;            // Array do mapa a ser renderizado
+        Texture tileAtlas;                  // Referencia a imagem, na memória, com todos os sprites
+        Vector2u initPlayerPosition;        // Posição inicial do jogador
+        vector<Vector2u> enemiesPositions;   // Lista coms as posições iniciais de cada grupo de ratos
+        int tileSize;                       // Tamaho da tela, para ajuste do aspect ratio
 
     public:
         TileMap(Texture &atlas, Vector2u screenSize);
 
-        bool loadMap(std::string &mapName);
+        // Operações entre tela e tileMap
+        Vector2u tileGridToPixel(int i, int j);
+        pair<int, int> pixelsToTileGrid(Vector2u position);
+
+        // Operações do tileMap
+        bool loadMap(const string &fileName);
         void draw(RenderWindow &window);
 
 };
