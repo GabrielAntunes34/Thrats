64f494bc8e64570953bec72d9c7c5e8cd08425f6
GabrielAntunes34 <82909777+GabrielAntunes34@users.noreply.github.com>
2024-12-09 22:47:42 -0300

    Updating sources from tile and tilemap

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -1,14 +1,114 @@
 #include "../include/TileMap.hpp"
 
+/*
+vector<vector<Tile>> tiles;  
+VertexArray vertexArray;                
+Texture tileAtlas;                      
+int tileSize;
+*/
+
+// Lê e já instancia os tiles do tileMap
+vector<vector<int>> readCsv(const string& fileName) {
+    ifstream file(fileName);
+    vector<vector<int>> contents(TILE_MAP_H, vector<int>(TILE_MAP_W));
+    string line;    // Auxiliar para ler cada linha do CSV
+    int i = 0, j = 0;       // Auxiliares para os index da matriz
+    
+    // Verificando se o arquivo foi aberto
+    if(!file.is_open()) {
+        cerr << "Um erro ocorreu ao ler o CSV: " << fileName << endl;
+        return {};
+    }
+
+    // Lendo os números do tileMap e guradando na matriz
+    string line;
+    while(getline(file, line)) {
+        stringstream ss(line);   // Iterador para parsear a string
+        string value;            // Auxiliar que mantem o valor lido
+
+        // Guardando os valores convertidos para inteiros na matriz
+        while(getline(ss, value, ',')) {
+            contents[i][j] = stoi(value);
+            i++;
+        }
+        j++;
+    }
+    file.close();
+
+    return contents;
+}
+
+// Auxiliar que adequa o tamanho de cada Tile as dimensões da tela
+int calculateTileSize(Vector2u screenSize) {
+    return min(screenSize.x / TILE_MAP_W, screenSize.y / TILE_MAP_H);
+}
+
+// Função que converte coordenadas no tile map em coordenadas da tela (pixels)
+Vector2u TileMap::tileGridToPixel(int i, int j) {
+    Vector2u screenPos;
+
+    screenPos.x = (int) i * this->tileSize + (this->tileSize / 2);
+    screenPos.y = (int) j * this->tileSize + (this->tileSize / 2);
+    return screenPos;
+}
+
+// Função que converte coordenadas no tile map em coordenadas da tela (pixels)
+pair<int, int> TileMap::pixelsToTileGrid(Vector2u position) {
+    pair<int, int> gridPos;
+
+    gridPos.first = (int) (position.x - (this->tileSize / 2)) / this->tileSize;
+    gridPos.second = (int) (position.y - (this->tileSize / 2)) / this->tileSize;
+    return gridPos;
+}
+
 // Construtor
 TileMap::TileMap(Texture &atlas, Vector2u screenSize) {
     // Inicializando as grades lógica e de renderização
-    this->tiles.assign(TILE_MAP_H, std::vector<Tile>(TILE_MAP_W, Tile()));
-    this->vertexArray.setPrimitiveType(Quads);
+    this->tiles.assign(TILE_MAP_H, vector<Tile>(TILE_MAP_W, Tile()));
+    //this->vertexArray.setPrimitiveType(Quads);
 
     // Salvando os parâmetros informados
     this->tileAtlas = atlas;
-    this->screenSize = screenSize;
+    this->tileSize = calculateTileSize(screenSize);
+    this->enemiesPositions = vector<Vector2u>();
 
     // LEMBRAR DE AJUSTAR O ASPECT RATIO OU NA MAIN, OU EM ALGUM ARQUIVO DE CONFIG
+}
+
+// Dado o nome de um CSV, essa função mapeaia todos os tiles lógicos e gráficos do nível
+bool TileMap::loadMap(const string &fileName) {
+    vector<vector<int>> csvMatrix;
+
+    // Lendo o CSV e instanciando os tiles lógicos (por enquanto)
+    csvMatrix = readCsv(fileName);
+    for(int i = 0; i < TILE_MAP_H; i++) {
+        for(int j = 0; j < TILE_MAP_W; j++) {
+            this->tiles[i][j].setId(csvMatrix[i][j]);
+
+            switch(this->tiles[i][j].getId()) {
+            case PLAYER:
+                this->initPlayerPosition = tileGridToPixel(i, j);
+                this->tiles[i][j].setSprite(); // sprite do chão
+                break;
+
+            case ENEMY:
+                this->enemiesPositions.push_back(Vector2u(i, j));
+                this->tiles[i][j].setSprite(); // Sprite de chão/ spawn do ratinho
+                break;
+            
+            case OBSTACLE:
+                this->tiles[i][j].setTaniblity(true);
+                this->tiles[i][j].setSprite();  // Sprite do obstáculo
+            
+                this->tiles[i][j].setSprite();  // Sprie do chão
+
+            default:
+                this->tiles[i][j].setSprite();  // Ground
+                break;
+            }
+        }
+    }
+
+
+
 }
